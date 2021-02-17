c3a31e5a8ca1a69ed8f5a7fc2f4ba36565f17ec6
Will Trinh <williamltrinh@gmail.com>
2021-02-16 18:51:03 -0800

    Implemented Home Feed

--- app.js
+++ app.js
@@ -3,22 +3,28 @@ $(document).ready(function(){
   $app.html('');
   // App Title
   var $title = $('<h1>Twiddler</h1>');
-  // Append title to the DOM
   $title.appendTo($app);
-  // Set a click event listener on the h1 element
-  $title.on("click", function(event) {
-    console.log(event);
-    alert('Title of this page is: ' + event.target.innerText);
+
+  // Update Feed Button
+  var $updateFeed = $('<button id="update-feed" type="button">Update Feed</button>');
+  $updateFeed.appendTo($app);
+
+  // Feed
+  var $feed = $('<div id="feed"></div>');
+  $feed.appendTo($app);
+
+  $updateFeed.on("click", function() {
+    // remove all previously existing tweets from feed
+    $feed.html('');
+    // create and append new tweets to the feed
+    var index = streams.home.length - 1;
+    while(index >= 0){
+      var tweet = streams.home[index];
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.appendTo($feed);
+      index -= 1;
+    }
   });
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    // most recent tweet that got pushed into the streams.home array
-    var tweet = streams.home[index];
-    // UI element ($ in front to notify human programmers that it is related to jQuery and not the backend)
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($app);
-    index -= 1;
-  }
 });
 
