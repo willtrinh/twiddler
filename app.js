0ac157eb492c2d71f3282059ff7d4f58bdf16b1b
Will Trinh <williamltrinh@gmail.com>
2021-02-16 19:42:22 -0800

    Implemented Tweet UI Components

--- app.js
+++ app.js
@@ -9,20 +9,34 @@ $(document).ready(function(){
   var $updateFeed = $('<button id="update-feed" type="button">Update Feed</button>');
 
   // Create event handler functions
-  var handleUpdateClick = function() {
+  var renderFeed = function() {
     $feed.html('');
     var index = streams.home.length - 1;
+    var iconsPath = 'assets/icons/placeholder.png';
     while(index >= 0){
       var tweet = streams.home[index];
       var $tweet = $('<div class="tweet"></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      var $iconsPath = 'assets/icons/placeholder.png';
+      // Tweets UI Components
+      var $profilePhoto = $('<img class="profile-photo">').attr('src', tweet.profilePhotoURL);
+      var $username = $('<span class="username"></span>').text('@' + tweet.user);
+      var $message = $('<p class="message"></p>').text(tweet.message);
+      var $timestamp = $('<span class="timestamp"></span>').text(tweet.created_at);
+      // Icons
+      var $comment = $('<img class="icon comment">').attr('src', iconsPath);
+      var $retweet = $('<img class="icon retweet">').attr('src', iconsPath);
+      var $like = $('<img class="icon like">').attr('src', iconsPath);
+      var $share = $('<img class="icon share">').attr('src', iconsPath);
+
+      $tweet.append($profilePhoto, $username, $message, $timestamp, $comment, $retweet, $like, $share);
       $tweet.appendTo($feed);
       index -= 1;
     }
   }
 
   // Set event listeners
-  $updateFeed.on("click", handleUpdateClick);
+  renderFeed();
+  $updateFeed.on("click", renderFeed);
 
   // Append new HTML elements to the DOM
   $title.appendTo($app);
